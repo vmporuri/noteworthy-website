e72111bf45bbe9567c3a3ac3423b2796bae0ae54
Venu Poruri <vmporuri@gmail.com>
2024-03-31 03:06:02 -0500

    refactor: adjust font sizes and colors slightly

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,10 @@ export function Intro() {
         alt="Vest Picture"
         className="h-3/4 rounded-3xl" />
       <div className="flex flex-col place-content-center gap-5 text-center w-1/2">
-        <h1 className="font-sans text-blue-400 text-6xl font-bold">Noteworthy</h1>
+        <h1 className="font-sans text-blue-400 text-8xl font-bold">Noteworthy</h1>
+        <h2 className="font-sans text-blue-400 text-4xl font-bold">
+          ♫&ensp;In-<i>vest</i>-ed in A Capella!&ensp;♫
+        </h2>
         <p className="font-sans text-lg">
           We’re a lower-voices a cappella group known for our fun performances and fresh takes on
           popular songs! It always feels sunny when we perform, and not just because of our golden
