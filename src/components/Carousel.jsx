1da1f3bb52686aadd5830804f9bd7e91139aedc4
Venu Poruri <vmporuri@gmail.com>
2024-03-31 19:21:23 -0500

    feat: add music videos

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,18 +14,23 @@ export default function Carousel({ slides }) {
 
   return (
     <div className="overflow-hidden relative w-1/5 h-96">
-      <img
-        src={slides[slideNum]}
-        className="h-full w-full object-cover"
-        alt={`Slide ${slideNum + 1}`}
-      />
-      <div className="absolute top-0 h-full w-full flex justify-between items-center text-white px-2 text-2xl">
-        <button onClick={prevSlide}>
-          <BsArrowLeftCircleFill />
-        </button>
-        <button onClick={nextSlide}>
-          <BsArrowRightCircleFill />
-        </button>
+      <h1 className="text-center text-blue-400 text-2xl font-bold pb-3">
+        Recent Events
+      </h1>
+      <div>
+        <img
+          src={slides[slideNum]}
+          className="h-full w-full object-cover"
+          alt={`Slide ${slideNum + 1}`}
+        />
+        <div className="absolute top-0 h-full w-full flex justify-between items-center text-white px-2 text-2xl">
+          <button onClick={prevSlide}>
+            <BsArrowLeftCircleFill />
+          </button>
+          <button onClick={nextSlide}>
+            <BsArrowRightCircleFill />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -5,17 +5,31 @@ export default function Media() {
     "https://noteworthy.studentorg.berkeley.edu/wp-content/uploads/2023/10/Halloween-Flyer-768x1086.png",
     "https://noteworthy.studentorg.berkeley.edu/wp-content/uploads/2023/11/West-Coast-Showcase-F23-Flyer.jpg",
     "https://noteworthy.studentorg.berkeley.edu/wp-content/uploads/2023/12/Flyer-NW-Show-F23-768x994.png",
-    "https://noteworthy.studentorg.berkeley.edu/wp-content/uploads/2024/01/Welcome-Back-Sp2024-768x994.png"
+    "https://noteworthy.studentorg.berkeley.edu/wp-content/uploads/2024/01/Welcome-Back-Sp2024-768x994.png",
   ];
 
   return (
-    <div id="media" className="h-fit pb-10">
-      <h1 className='text-center text-5xl text-blue-400 font-bold pb-6'>Media</h1>
+    <div id="media" className="h-fit pb-20">
+      <h1 className="text-center text-5xl text-blue-400 font-bold pb-6">
+        Media
+      </h1>
       <div className="flex place-content-evenly">
-        <Carousel
-          slides={flyers}
-        />
+        <Carousel slides={flyers} />
+        <div className="h-96 aspect-video flex flex-col">
+          <h1 className="text-center text-blue-400 text-2xl font-bold pb-3">
+            Videos
+          </h1>
+          <iframe
+            src="https://www.youtube.com/embed/n0p8MxT8AxI"
+            title='Noteworthy "Bet On It", Zac Efron - Welcome Back to A Cappella Fall 2022'
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            className="aspect-video flex-1"
+          ></iframe>
+        </div>
       </div>
     </div>
-  )
+  );
 }
